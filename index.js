dddd58a31ec519076f8bc931504965e65d36c8f9
sudhanshupandey7294 <sudhanshupandey738@gmail.com>
2025-07-03 09:54:24 +0530

    backend fresh

--- index.js
+++ index.js
@@ -36,6 +36,11 @@ app.use('/api/triage', triageRoutes);
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
 
+app.get('/', (req, res) => {
+  res.send('✅ AI Triage Backend is Live!');
+});
+
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   // useNewUrlParser: true,
